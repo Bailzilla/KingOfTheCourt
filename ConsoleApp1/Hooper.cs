fd417f77e41825c0826788811d39614641245fef
Keith Bailey <bailey.keith.allen@gmail.com>
2021-03-06 12:44:55 -0500

    Add project files.

---
+++ ConsoleApp1/Hooper.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace KingOfTheCourt
+{
+    class Hooper
+    {
+        public string name;
+        public int accuracy = 5;
+        public bool isTurn = true;
+        public int score = 0;
+        public int attempts = 0;
+
+ 
+        public Hooper(string playerName)
+        {
+            name = playerName;
+        }
+        public Hooper(string playerName, int playerAcc)
+        {
+            name = playerName;
+            accuracy = playerAcc;
+        }
+        public bool Shoot()
+        {
+            System.Random random = new System.Random();
+
+            if (random.Next(10) < accuracy)
+            {
+                return true;
+            } 
+            else
+            {
+                return false;
+            }
+        }
+        public void madeShot()
+        {
+            score++;
+            attempts++;
+            Console.WriteLine($"{name} Attempt:{attempts}. Points:{score}");
+        }
+
+        public void missedShot()
+        {
+            attempts++;
+            isTurn = false;
+            Console.WriteLine($"{name} Miss");
+        }
+    }
+}
---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace KingOfTheCourt
+{
+    class Game
+    {
+        static void Main(string[] args)
+        {
+            
+            Hooper player = new Hooper("Keith");
+            Hooper player2 = new Hooper("Anne");
+
+            while(player.score < 12 && player2.score < 12 )
+            {
+                if (player.isTurn)
+                {
+                    bool result1 = player.Shoot();
+                    if (result1)
+                    {
+                        player.madeShot();
+                    }
+                    else
+                    {
+                        player.missedShot();
+                        player2.isTurn = true;
+                    }
+                }
+                else if(player2.isTurn)
+                {
+                    bool result2 = player2.Shoot();
+                    if (result2)
+                    {
+                        player2.madeShot();
+                    }
+                    else
+                    {
+                        player2.missedShot();
+                        player.isTurn = true;
+                    }
+                }
+             }
+
+        }
+    }
+}
