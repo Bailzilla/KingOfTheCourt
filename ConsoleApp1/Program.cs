ef549aeed67f8ce092e357937f7deb9f03d082c7
Keith Bailey <bailey.keith.allen@gmail.com>
2021-03-07 09:28:36 -0500

    adding master loop allowing play again or quit

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -7,38 +7,52 @@ namespace KingOfTheCourt
         static void Main(string[] args)
         {
             
-            Hooper player = new Hooper("Keith");
-            Hooper player2 = new Hooper("Anne");
+          
+            var answer = "1";
 
-            while(player.score < 12 && player2.score < 12 )
-            {
-                if (player.isTurn)
+            while (answer == "1") {
+
+                Hooper player = new Hooper("Keith");
+                Hooper player2 = new Hooper("Anne");
+
+                while (player.score < 12 && player2.score < 12)
                 {
-                    bool result1 = player.Shoot();
-                    if (result1)
+                    if (player.isTurn)
                     {
-                        player.madeShot();
+                        bool result1 = player.Shoot();
+                        if (result1)
+                        {
+                            player.madeShot();
+                        }
+                        else
+                        {
+                            player.missedShot();
+                            player2.isTurn = true;
+                        }
                     }
-                    else
+                    else if (player2.isTurn)
                     {
-                        player.missedShot();
-                        player2.isTurn = true;
+                        bool result2 = player2.Shoot();
+                        if (result2)
+                        {
+                            player2.madeShot();
+                        }
+                        else
+                        {
+                            player2.missedShot();
+                            player.isTurn = true;
+                        }
                     }
+
+
                 }
-                else if(player2.isTurn)
-                {
-                    bool result2 = player2.Shoot();
-                    if (result2)
-                    {
-                        player2.madeShot();
-                    }
-                    else
-                    {
-                        player2.missedShot();
-                        player.isTurn = true;
-                    }
-                }
-             }
+
+                Console.WriteLine("Press 1 then hit enter to play again. Press anything else then enter to exit.");
+                answer = Console.ReadLine();
+
+            }
+            
+            
 
         }
     }
